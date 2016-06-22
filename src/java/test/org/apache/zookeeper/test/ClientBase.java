44cb1167722602d751c3bba98dc1a2327fa6d8fa
Michael Han <hanm@cloudera.com>
2016-06-22 08:20:02 -0700

    Thread dump.

--- src/java/test/org/apache/zookeeper/test/ClientBase.java
+++ src/java/test/org/apache/zookeeper/test/ClientBase.java
@@ -25,6 +25,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
@@ -259,6 +261,27 @@ public abstract class ClientBase extends ZKTestCase {
                 // ignore
             }
         }
+
+        final StringBuilder dump = new StringBuilder();
+        ThreadInfo[] threadInfos = ManagementFactory.getThreadMXBean()
+                .dumpAllThreads(true, true);
+
+        for (ThreadInfo threadInfo : threadInfos) {
+            dump.append('"');
+            dump.append(threadInfo.getThreadName());
+            dump.append("\" ");
+            final Thread.State state = threadInfo.getThreadState();
+            dump.append("\n java.lang.Thread.State: ");
+            dump.append(state);
+            final StackTraceElement[] stackTraceElements = threadInfo.getStackTrace();
+            for (final StackTraceElement stackTraceElement : stackTraceElements) {
+                dump.append("\n        at  ");
+                dump.append(stackTraceElement);
+            }
+            dump.append("\n\n");
+        }
+        LOG.info("Stack dump \n" + dump);
+        
         return false;
     }
 
