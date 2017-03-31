f89f29f1b16b58fe00582b765289622af79a7e2f
Michael Han <hanm@apache.org>
2017-03-30 22:14:39 -0700

    Add thread dump for bean unregister.

--- src/java/main/org/apache/zookeeper/jmx/MBeanRegistry.java
+++ src/java/main/org/apache/zookeeper/jmx/MBeanRegistry.java
@@ -19,11 +19,14 @@
 package org.apache.zookeeper.jmx;
 
 import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
 import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
 import java.util.HashSet;
 import java.util.concurrent.ConcurrentHashMap;
+
+import javax.management.MBeanServerConnection;
 
 import javax.management.JMException;
 import javax.management.MBeanServer;
@@ -120,8 +123,29 @@ public class MBeanRegistry {
      * @param bean
      */
     private void unregister(String path,ZKMBeanInfo bean) throws JMException  {
-        if(path==null)
-            return;
+        if(path==null) {
+          LOG.info("valkyrie: bean unregister path is null!!!!");
+          final StringBuilder dump = new StringBuilder();
+          ThreadInfo[] threadInfos = ManagementFactory.getThreadMXBean()
+              .dumpAllThreads(true, true);
+
+          for (ThreadInfo threadInfo : threadInfos) {
+            dump.append('"');
+            dump.append(threadInfo.getThreadName());
+            dump.append("\" ");
+            final Thread.State state = threadInfo.getThreadState();
+            dump.append("\n java.lang.Thread.State: ");
+            dump.append(state);
+            final StackTraceElement[] stackTraceElements = threadInfo.getStackTrace();
+            for (final StackTraceElement stackTraceElement : stackTraceElements) {
+              dump.append("\n        at  ");
+              dump.append(stackTraceElement);
+            }
+            dump.append("\n\n");
+          }
+          LOG.info("Stack dump \n" + dump);
+          return;
+        }
         if (!bean.isHidden()) {
             final ObjectName objName = makeObjectName(path, bean);
             if (LOG.isInfoEnabled()) {
